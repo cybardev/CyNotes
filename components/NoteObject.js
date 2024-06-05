0aba3659403408335f88e201577e4e5203d912e5
cybardev <sheikh@cybar.dev>
2024-06-05 12:28:06 -0300

    feat: create type definition for note object

---
+++ components/NoteObject.js
@@ -0,0 +1,8 @@
+/**
+ * Object to store note data
+ *
+ * @typedef {Object} NoteObject
+ * @property {string} id unique identifier for the note
+ * @property {string} title title of the note
+ * @property {string} content content of the note
+ */
